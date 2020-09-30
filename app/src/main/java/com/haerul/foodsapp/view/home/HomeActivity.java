fc33b3f346aa1980addfef58c548865f041e2fd4
nhoibk99 <nhoibk99@gmail.com>
2020-09-30 13:27:56 +0700

    Done Lab 1

--- app/src/main/java/com/haerul/foodsapp/view/home/HomeActivity.java
+++ app/src/main/java/com/haerul/foodsapp/view/home/HomeActivity.java
@@ -9,9 +9,11 @@ package com.haerul.foodsapp.view.home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -27,33 +29,60 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-// TODO 31 implement the HomeView interface at the end
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements HomeView {
 
-    /*
-     * TODO 32 Add @BindView Annotation (1)
-     *
-     * 1. viewPagerHeader
-     * 2. recyclerCategory
-     *
-     */
 
-    /*
-     *  TODO 33 Create variable for presenter;
-     */
+    @BindView(R.id.viewPagerHeader) ViewPager viewPagerMeal;
+    @BindView(R.id.recyclerCategory) RecyclerView recyclerViewCategory;
+
+
+    HomePresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        /*
-         *  TODO 34 bind the ButterKnife (2)
-         */
+        ButterKnife.bind(this);
+        presenter = new HomePresenter(this);
+        presenter.getMeals();
+        presenter.getCategories();
 
-        /*
-         *  TODO 35 Declare the presenter
-         *  new HomePresenter(this)
-         */
+    }
+
+    @Override
+    public void showLoading() {
+        findViewById(R.id.shimmerMeal).setVisibility(View.VISIBLE);
+        findViewById(R.id.shimmerCategory).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideLoading() {
+        findViewById(R.id.shimmerMeal).setVisibility(View.GONE);
+        findViewById(R.id.shimmerCategory).setVisibility(View.GONE);
+    }
+
+    @Override
+    public void setMeal(List<Meals.Meal> meal) {
+        ViewPagerHeaderAdapter headerAdapter = new ViewPagerHeaderAdapter(meal, this);
+        viewPagerMeal.setAdapter(headerAdapter);
+        viewPagerMeal.setPadding(20,0,150,0);
+        headerAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void setCategories(List<Categories.Category> category) {
+        RecyclerViewHomeAdapter homeAdapter = new RecyclerViewHomeAdapter(category,this);
+         recyclerViewCategory.setAdapter(homeAdapter);
+         GridLayoutManager layoutManager = new GridLayoutManager(this,3,
+                 GridLayoutManager.VERTICAL,false);
+         recyclerViewCategory.setLayoutManager(layoutManager);
+         recyclerViewCategory.setNestedScrollingEnabled(true);
+         homeAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onErrorLoading(String message) {
+        Utils.showDialogMessage(this,"title",message);
     }
 
     // TODO 36 Overriding the interface
